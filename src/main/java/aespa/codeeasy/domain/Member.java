c850fa5b599c2c3fff68ed1e3cac2174f03ca82a
geonho lee <lgh8079@gmail.com>
2024-05-01 16:51:03 +0900

    first commit

---
+++ src/main/java/aespa/codeeasy/domain/Member.java
@@ -0,0 +1,24 @@
+package aespa.codeeasy.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter @Setter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nickname;
+    private String email;
+    private String password;
+    private String tier = "bronze"; // 티어 기본값 설정/
+
+
+}
