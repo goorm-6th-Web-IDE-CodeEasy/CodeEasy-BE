7069eda2b6eafbbe5fb59b7610fb33a0e4bd96d0
geonho lee <lgh8079@gmail.com>
2024-05-01 16:13:23 +0900

    Resolve conflicts

---
+++ src/main/java/aespa/codeeasy/CodeeasyApplication.java
@@ -0,0 +1,13 @@
+package aespa.codeeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aespa/codeeasy/CodeeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package aespa.codeeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
