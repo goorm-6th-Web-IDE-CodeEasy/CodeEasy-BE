a5870f315fa227a8641a007be372c1aa0b91e67a
CJ <choijun0627@konkuk.ac.kr>
2024-05-02 22:39:14 +0900

    Feat : 이메일 인증 중 이메일 설정 로직 구현
    이메일 보낼 때 필요한 정보 설정하는 createMail 메서드 구현
    발신, 수신 이메일, 메인 내용 등 설정

--- src/main/java/aespa/codeeasy/service/MailService.java
+++ src/main/java/aespa/codeeasy/service/MailService.java
@@ -1,13 +1,20 @@
 package aespa.codeeasy.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMessage.RecipientType;
 import java.util.Random;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MailService {
 
+    private final JavaMailSender javaMailSender;
     private static Integer certificationCode;
 
     private void createCertificationCode(){
@@ -23,6 +30,30 @@ public class MailService {
         certificationCode = Integer.parseInt(builder.toString());
     }
 
+    public MimeMessage createMail(String email){
+        createCertificationCode();
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        String fromMail="choijun0627@gmail.com";
+        String toMail=email;
+        String emailTitle="CodeEasy 회원 가입 인증 번호";
+        String emailContent= "";
+        emailContent += "<h3>" + "요청하신 인증 번호입니다." + "</h3>";
+        emailContent += "<h1>" + certificationCode + "</h1>";
+        emailContent += "<h3>" + "감사합니다." + "</h3>";
+
+        try {
+            message.setFrom(fromMail);
+            message.setRecipients(RecipientType.TO, toMail);
+            message.setSubject(emailTitle);
+            message.setText(emailContent,"UTF-8","html");
+        } catch (MessagingException messagingException) {
+            log.error("이메일 전송 관련 예외 발생. 수신 이메일: {}, 예외: {}", toMail, messagingException);
+        }
+
+        return message;
+    }
     public void sendEmail(String email) {
 
     }
