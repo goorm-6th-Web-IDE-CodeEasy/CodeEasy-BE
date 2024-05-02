d8a0349c4b4dce8291c3277c3e212012fe31befd
CJ <choijun0627@konkuk.ac.kr>
2024-05-02 21:41:57 +0900

    Feat : 이메일 인증 인증번호 생성 로직 구현
    이메일 인증 서비스에 인증번호 생성 로직 구현

--- src/main/java/aespa/codeeasy/service/MailService.java
+++ src/main/java/aespa/codeeasy/service/MailService.java
@@ -1,6 +1,28 @@
 package aespa.codeeasy.service;
 
+import java.util.Random;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MailService {
+
+    private static Integer certificationCode;
+
+    private void createCertificationCode(){
+        int certificationCodeLength = 6;
+
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder();
+
+        for(int length = 0; length < certificationCodeLength; length++) {
+            builder.append(random.nextInt(10));
+        }
+
+        certificationCode = Integer.parseInt(builder.toString());
+    }
+
     public void sendEmail(String email) {
 
     }
