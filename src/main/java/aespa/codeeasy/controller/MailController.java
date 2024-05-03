a43912478134b1bd6f6a3b20a842baa8f0225037
CJ-1998 <96980115+CJ-1998@users.noreply.github.com>
2024-05-03 18:46:41 +0900

    [준] 회원 가입 이메일 인증 구현 (#4)
    * Feat : 이메일 인증 번호 발송 컨트롤러 구현
    
    이메일 인증 요청을 받아서 이메일 인증 번호 발송하는 컨트롤러 구현
    이메일 인증 번호 발송 로직이 있는 MailService 클래스 생성
    
    * Feat : 이메일 인증 인증번호 생성 로직 구현
    
    이메일 인증 서비스에 인증번호 생성 로직 구현
    
    * Feat : 이메일 인증 중 이메일 설정 로직 구현
    
    이메일 보낼 때 필요한 정보 설정하는 createMail 메서드 구현
    발신, 수신 이메일, 메인 내용 등 설정
    
    * Feat : 이메일 인증 이메일 발송 로직 구현
    
    이메일 인증부분에서 이메일 발송하는 메서드 구현
    
    * Chore : build.gradle 수정
    
    원활한 postman을 통한 api 테스트를 위해 build.gradle 수정
    스프링 시큐리티 관련 dependency 모두 주석 처리
    
    * Chore : Redis 사용을 위한 설정 값 추가
    
    Redis 사용 위해 build.gradle에 dependency 추가
    application.properties에 필요한 값 설정
    
    * Feat : Redis 사용 위한 util 클래스 생성
    
    Redis를 사용하는데 필요한 util 클래스인 RedisUtil 클래스 생성
    Redis에 데이터 저장하고 조회하고 삭제하는 등 기능 수행
    
    * Feat : 이메일 인증에 필요한 DTO 클래스 생성
    
    이메일 인증에서 인증 번호 확인할 때 인증 번호 받아오는 DTO 클래스 생성
    EmailCheckDto로 email, 인증번호 받아옴
    
    * Style : 이메일 인증 DTO 클래스 필드명 수정
    
    인증 번호 받는 필드 authNum에서 certificationCode로 필드명 수정
    
    * Feat : 인증 번호 확인 요청 받는 컨트롤러 구현
    
    이메일 인증에서 인증 번호 확인 요청 받는 컨트롤러 구현
    email, 인증 번호 넘겨야 함
    인증 번호가 같다면 200 OK와 true 반환
    인증 번호가 다르다면 200 OK와 false 반환
    
    * Feat : 인증 번호 확인 로직 구현
    
    이메일 설정하는 메서드에서 인증 번호 - email을 Redis에 저장
    이때 인증 시간은 3분으로 설정 후 저장
    인증 번호 확인 로직은 Redis에서 인증 번호 있는지 확인
    있다면 value가 email인지 확인 후 true 반환
    
    * Feat : 이메일 인증 번호 발송 컨트롤러 수정
    
    이메일 인증 번호 발송 컨트롤러 파라미터로 넘어오는 값 dto로 받도록 수정
    원래는 RequestParam 사용했음
    EmailRequestDto 클래스 생성 후 dto로 받도록 컨트롤러 수정

--- src/main/java/aespa/codeeasy/controller/MailController.java
+++ src/main/java/aespa/codeeasy/controller/MailController.java
@@ -1,4 +1,36 @@
 package aespa.codeeasy.controller;
 
+import aespa.codeeasy.dto.EmailCheckDto;
+import aespa.codeeasy.dto.EmailRequestDto;
+import aespa.codeeasy.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class MailController {
+
+    private final MailService mailService;
+
+    @PostMapping("/register/send-certification")
+    public ResponseEntity sendEmail(@RequestBody EmailRequestDto emailRequestDto) {
+        mailService.sendEmail(emailRequestDto.getEmail());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/register/certificate-code")
+    public ResponseEntity<Boolean> checkCertificationCode(@RequestBody EmailCheckDto emailCheckDto) {
+        Boolean isValidCertificationCode = mailService.checkCertificationCode(emailCheckDto.getEmail(),
+                emailCheckDto.getCertificationCode());
+
+        if (isValidCertificationCode) {
+            return ResponseEntity.ok(true); // 200 OK와 함께 true 반환
+        } else {
+            return ResponseEntity.ok(false); // 200 OK와 함께 false 반환
+        }
+    }
 }
