b0b5c20de1c48af17c98a24b53792ac2b1c5f7f2
CJ <choijun0627@konkuk.ac.kr>
2024-05-03 17:53:21 +0900

    Feat : 인증 번호 확인 요청 받는 컨트롤러 구현
    이메일 인증에서 인증 번호 확인 요청 받는 컨트롤러 구현
    email, 인증 번호 넘겨야 함
    인증 번호가 같다면 200 OK와 true 반환
    인증 번호가 다르다면 200 OK와 false 반환

--- src/main/java/aespa/codeeasy/controller/MailController.java
+++ src/main/java/aespa/codeeasy/controller/MailController.java
@@ -1,11 +1,12 @@
 package aespa.codeeasy.controller;
 
+import aespa.codeeasy.dto.EmailCheckDto;
 import aespa.codeeasy.service.MailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,20 @@ public class MailController {
     private final MailService mailService;
 
     @PostMapping("/register/send-certification")
-    public ResponseEntity sendEmail(@RequestParam("email") String email){
+    public ResponseEntity sendEmail(@RequestParam("email") String email) {
         mailService.sendEmail(email);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/register/certificate-code")
+    public ResponseEntity<Boolean> checkCertificationCode(@RequestBody EmailCheckDto emailCheckDto) {
+        Boolean isValidCertificationCode = mailService.checkCertificationCode(emailCheckDto.getEmail(),
+                emailCheckDto.getCertificationCode());
+
+        if (isValidCertificationCode) {
+            return ResponseEntity.ok(true); // 200 OK와 함께 true 반환
+        } else {
+            return ResponseEntity.ok(false); // 200 OK와 함께 false 반환
+        }
     }
 }
--- src/main/java/aespa/codeeasy/service/MailService.java
+++ src/main/java/aespa/codeeasy/service/MailService.java
@@ -17,28 +17,28 @@ public class MailService {
     private final JavaMailSender javaMailSender;
     private static Integer certificationCode;
 
-    private void createCertificationCode(){
+    private void createCertificationCode() {
         int certificationCodeLength = 6;
 
         Random random = new Random();
         StringBuilder builder = new StringBuilder();
 
-        for(int length = 0; length < certificationCodeLength; length++) {
+        for (int length = 0; length < certificationCodeLength; length++) {
             builder.append(random.nextInt(10));
         }
 
         certificationCode = Integer.parseInt(builder.toString());
     }
 
-    public MimeMessage createMail(String email){
+    public MimeMessage createMail(String email) {
         createCertificationCode();
 
         MimeMessage message = javaMailSender.createMimeMessage();
 
-        String fromMail="choijun0627@gmail.com";
-        String toMail=email;
-        String emailTitle="CodeEasy 회원 가입 인증 번호";
-        String emailContent= "";
+        String fromMail = "choijun0627@gmail.com";
+        String toMail = email;
+        String emailTitle = "CodeEasy 회원 가입 인증 번호";
+        String emailContent = "";
         emailContent += "<h3>" + "요청하신 인증 번호입니다." + "</h3>";
         emailContent += "<h1>" + certificationCode + "</h1>";
         emailContent += "<h3>" + "감사합니다." + "</h3>";
@@ -47,15 +47,20 @@ public class MailService {
             message.setFrom(fromMail);
             message.setRecipients(RecipientType.TO, toMail);
             message.setSubject(emailTitle);
-            message.setText(emailContent,"UTF-8","html");
+            message.setText(emailContent, "UTF-8", "html");
         } catch (MessagingException messagingException) {
             log.error("이메일 전송 관련 예외 발생. 수신 이메일: {}, 예외: {}", toMail, messagingException);
         }
 
         return message;
     }
+
     public void sendEmail(String email) {
         MimeMessage message = createMail(email);
         javaMailSender.send(message);
+    }
+
+    public Boolean checkCertificationCode(String email, String certificationCode) {
+        return null;
     }
 }
