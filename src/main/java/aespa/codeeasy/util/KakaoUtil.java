293c3bae3aa8817fdd99646b92a47a4828ccdcd9
CJ <choijun0627@konkuk.ac.kr>
2024-05-07 15:22:05 +0900

    Feat : 카카오 로그인 access token 받는 메서드 구현
    카카오 로그인에서 인가 코드로 access token 받는 메서드 구현
    HTTP 요청 직접 만들어서 보내고 응답 받음
    응답에 access token 있어서 json 파싱해서 access token 받음

--- src/main/java/aespa/codeeasy/util/KakaoUtil.java
+++ src/main/java/aespa/codeeasy/util/KakaoUtil.java
@@ -1,9 +1,22 @@
 package aespa.codeeasy.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KakaoUtil {
@@ -17,6 +30,47 @@ public class KakaoUtil {
     public String getKakaoUrl() {
         return "https://kauth.kakao.com/oauth/authorize?response_type=code"
                 + "&client_id=" + client_id + "&redirect_uri=" + redirect_uri;
+    }
+
+    public String getAccessToken(String code) {
+        String accessToken = "";
+        String refreshToken = "";
+        String reqUrl = "https://kauth.kakao.com/oauth/token";
+
+        // 1. header 생성
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // 2. body 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code"); //고정값
+        params.add("client_id", client_id);
+        params.add("redirect_uri", redirect_uri); //등록한 redirect uri
+        params.add("code", code);
+
+        // 3. header + body
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(params, httpHeaders);
+
+        // 4. http 요청하기
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(
+                reqUrl,
+                HttpMethod.POST,
+                httpEntity,
+                String.class
+        );
+
+        // 5. 응답으로 온 json 파싱
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode root = mapper.readTree(response.getBody());
+            accessToken = root.path("access_token").asText();
+            refreshToken = root.path("refresh_token").asText();
+        } catch (JsonProcessingException jsonProcessingException) {
+            log.error("Json 파싱 관련 예외 발생. 예외: {}", jsonProcessingException);
+        }
+
+        return accessToken;
     }
 
 }
