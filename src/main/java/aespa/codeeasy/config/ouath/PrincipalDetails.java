6376a14c8268ae43f9d9706499f39618a35ed4bb
geonho lee <lgh8079@gmail.com>
2024-05-08 14:24:40 +0900

    깃 로그인 구현중, 로그아웃 구현

---
+++ src/main/java/aespa/codeeasy/config/ouath/PrincipalDetails.java
@@ -0,0 +1,43 @@
+package aespa.codeeasy.config.ouath;
+
+import aespa.codeeasy.domain.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+public class PrincipalDetails implements OAuth2User {
+    private Member member;
+    private Map<String, Object> attributes;
+
+    // OAuth 로그인
+
+    public PrincipalDetails(Member member, Map<String, Object> attributes) {
+        this.member = member;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collet = new ArrayList<GrantedAuthority>();
+        collet.add(()-> member.getRole());
+        return collet;
+    }
+
+    @Override
+    public String getName() {
+        return member.getId() + "";
+    }
+}
--- src/main/java/aespa/codeeasy/domain/Member.java
+++ src/main/java/aespa/codeeasy/domain/Member.java
@@ -12,6 +12,7 @@ import lombok.*;
 @Builder
 @Getter @Setter
 public class Member {
+    private String role; // 또는 어떤 타입이든 간에 role을 저장하는 필드
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,5 +22,11 @@ public class Member {
     private String password;
     private String tier = "bronze"; // 티어 기본값 설정
 
+    public String getRole() {
+        return this.role;
+    }
 
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
