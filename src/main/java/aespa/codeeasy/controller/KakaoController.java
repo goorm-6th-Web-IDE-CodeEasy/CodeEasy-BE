848644d977d7f728382948e58ad20023a8b3f819
CJ <choijun0627@konkuk.ac.kr>
2024-05-11 18:11:52 +0900

    Fix : 카카오 소셜 로그인 위한 이전 클래스들 수정
    이전 카카오 소셜 로그인 위해 만들었던 클래스들 annotation 주석 처리
    이전 클래스들 있어서 오류 발생해 주석 처리함

--- src/main/java/aespa/codeeasy/controller/KakaoController.java
+++ src/main/java/aespa/codeeasy/controller/KakaoController.java
@@ -8,12 +8,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
-@Controller
+//@Controller
 @RequiredArgsConstructor
 public class KakaoController {
 
