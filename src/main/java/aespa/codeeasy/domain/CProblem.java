1706cfbc17a41fd42045fd51003bfe5f948936e4
CJ <choijun0627@konkuk.ac.kr>
2024-05-13 21:20:34 +0900

    Feat : 문제 도메인 필드 추가
    문제 도메인에 필요한 필드들 추가

--- src/main/java/aespa/codeeasy/domain/CProblem.java
+++ src/main/java/aespa/codeeasy/domain/CProblem.java
@@ -1,4 +1,30 @@
 package aespa.codeeasy.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class CProblem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String problemTitle;
+    private String problemContent;
+    private String problemInputContent;
+    private String problemOutputContent;
+
+    private String algorithm;
+    private String tier;
+
+    private Long timeLimit;
+    private Long memoryLimit;
+    
 }
