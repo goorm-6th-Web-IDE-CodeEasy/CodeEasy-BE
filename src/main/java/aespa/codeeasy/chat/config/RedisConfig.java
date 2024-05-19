ab47abd51e1459d83792bcd0aa077aff6eb5599f
YJK-dev <90750483+YJK-dev@users.noreply.github.com>
2024-05-20 07:58:37 +0900

    * Fix : Redis 설정 수정
    Redis pub/sub 메시지를 처리하는 listener 설정 수정
    중복되는 RedisConfig 삭제

--- src/main/java/aespa/codeeasy/chat/config/RedisConfig.java
+++ src/main/java/aespa/codeeasy/chat/config/RedisConfig.java
@@ -1,23 +1,29 @@
 package aespa.codeeasy.chat.config;
 
+import aespa.codeeasy.chat.pubsub.RedisSubscriber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.Map;
+
 @Configuration
 public class RedisConfig {
+    private static final String MAIN_ROOM_ID = "main-room";
 
     /**
      * redis pub/sub 메시지를 처리하는 listener 설정
      */
     @Bean
-    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory) {
+    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory, RedisSubscriber redisSubscriber) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(redisSubscriber, new ChannelTopic(MAIN_ROOM_ID));
         return container;
     }
 
@@ -32,4 +38,10 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
         return redisTemplate;
     }
+
+    @Bean
+    public ChannelTopic channelTopic() {
+        return new ChannelTopic(MAIN_ROOM_ID);
+    }
+
 }
