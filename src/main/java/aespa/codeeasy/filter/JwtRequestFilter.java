ae3fd51c006c7ac39b0799d658ee33dd98a4aec7
YJK-dev <90750483+YJK-dev@users.noreply.github.com>
2024-05-20 10:20:13 +0900

    [진경] 회원 조회, 채팅 관련 구현 및 수정  (#16)
    * * Fix : Redis 설정 수정
    
    Redis pub/sub 메시지를 처리하는 listener 설정 수정
    중복되는 RedisConfig 삭제
    
    * * Fix : Jwt 관련 수정
    
    사용하지 않는 JwtRequestFilter, JwtTokenUtil 주석처리
    
    * * Feat : 채팅 관련 로직 구현 및 수정
    
    ChatController API 경로 설정 수정
    ChatRoomRepository 채팅 관련 로직 추가
    
    * * Feat : 토큰 기반으로 로그인한 유저 정보 조회 기능 추가
    
    Authorization 헤더를 통해 토큰을 받아 현재 유저 정보를 반환하는 기능 추가
    getUserFromToken에서 JWT 토큰을 이용해 이메일을 추출하고,
    해당 이메일로 유저 정보를 조회하도록 구현
    이메일을 찾을 수 없거나 유저 정보가 없을 경우 예외 처리 추가
    로그를 통해 디버깅 정보 출력

--- src/main/java/aespa/codeeasy/filter/JwtRequestFilter.java
+++ src/main/java/aespa/codeeasy/filter/JwtRequestFilter.java
@@ -1,7 +1,5 @@
 package aespa.codeeasy.filter;
 
-import aespa.codeeasy.util.JwtBlacklistUtil;
-import aespa.codeeasy.util.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,44 +16,44 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
-public class JwtRequestFilter extends OncePerRequestFilter {
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
-
-    @Autowired
-    private JwtBlacklistUtil jwtBlacklistUtil;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-            throws ServletException, IOException {
-        final String authorizationHeader = request.getHeader("Authorization");
-
-        String username = null;
-        String jwt = null;
-
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            jwt = authorizationHeader.substring(7);
-            if (jwtBlacklistUtil.isTokenBlacklisted(jwt)) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
-            username = jwtTokenUtil.extractUsername(jwt);
-        }
-
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-            if (jwtTokenUtil.validateToken(jwt, userDetails.getUsername())) {
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken
-                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-            }
-        }
-        chain.doFilter(request, response);
-    }
-}
+//@Component
+//public class JwtRequestFilter extends OncePerRequestFilter {
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Autowired
+//    private JwtTokenUtil jwtTokenUtil;
+//
+//    @Autowired
+//    private JwtBlacklistUtil jwtBlacklistUtil;
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+//            throws ServletException, IOException {
+//        final String authorizationHeader = request.getHeader("Authorization");
+//
+//        String username = null;
+//        String jwt = null;
+//
+//        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+//            jwt = authorizationHeader.substring(7);
+//            if (jwtBlacklistUtil.isTokenBlacklisted(jwt)) {
+//                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//                return;
+//            }
+//            username = jwtTokenUtil.extractUsername(jwt);
+//        }
+//
+//        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+//            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+//            if (jwtTokenUtil.validateToken(jwt, userDetails.getUsername())) {
+//                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+//                        userDetails, null, userDetails.getAuthorities());
+//                usernamePasswordAuthenticationToken
+//                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+//            }
+//        }
+//        chain.doFilter(request, response);
+//    }
+//}
